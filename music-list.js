3036a8ebf9e30523ae402e458e2d2ee739072dd2
NAR0107 <105336404+NAR0107@users.noreply.github.com>
2023-07-09 15:00:13 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+  {
+    name: "Naa Roja Nevey - Home",
+    artist: "Hesham Abdul",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ori Vari",
+    artist: "Santosh Narayana",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Tere Vaaste",
+    artist: "Amitabh Bhattacharya",
+    img: "tere",
+    src: "tere"
+  },
+  {
+    name: "Sakkarakatti",
+    artist: "Hip hOP Tamizha",
+    img: "meesa",
+    src: "meesa"
+  },
+  {
+    name: "I Am A Rider",
+    artist: "Imran Khan",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Joker Thiller",
+    artist: "Zazie Beetz",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Darshan (Hridayam)",
+    artist: "Hemash Abdul Wahab",
+    img: "darsha",
+    src: "darsha"
+  },
+];
